89cf5b801439754e80d543b452686b303830d345
mrk-han <mark.han@willowtreeapps.com>
2022-02-14 02:47:45 -0500

    draw label and button from viewcontroller   - implement incrementCount method

---
+++ ClickCounter/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  ClickCounter
+//
+//  Created by Mark Han on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var count = 0
+    var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // label
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y:150, width:60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        self.label = label
+        
+        //button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y:250, width: 60, height: 60)
+        button.setTitle("click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
+    }
+    
+    @objc func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+    }
+
+}
+
